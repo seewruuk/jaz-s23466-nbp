2d842b012b653a3fbe10170dd656f48bf8d5d343
seewruuk <sewruczekk@icloud.com>
2023-06-24 15:14:07 +0200

    test

---
+++ src/main/java/com/example/jazs23466nbp/JazS23466NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs23466nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS23466NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS23466NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jazs23466nbp/JazS23466NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs23466nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS23466NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
